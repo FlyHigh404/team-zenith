00b1f9313d2b4fd9ef558ff938e9ca7ab742b398
rafienajwan <rafie.anjasmara@gmail.com>
2025-03-23 21:49:47 +0700

    refactor(migrations): hapus tabel lama dan tambahkan tabel user serta autentikasi

---
+++ database/migrations/2025_03_22_135135_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->char('password', 30);
+            $table->char('email', 30);
+            $table->char('username', 20);
+            $table->char('nama', 30);
+            $table->enum('status', ['available', 'notavailable']);
+            $table->string('desc', 500)->nullable();
+            $table->date('birthdate');
+            $table->char('fotoProfil', 100)->nullable();
+            $table->char('lokasi', 20);
+            $table->string('notelp', 25);
+            $table->enum('levelProfesional', ['1', '2', '3']);
+            $table->enum('keahlian', ['plate', 'pipe']);
+            $table->dateTime('createdAt');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
