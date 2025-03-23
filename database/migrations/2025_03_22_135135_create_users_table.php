94867f67e1ee94d310e66ba556ccb5367691b86c
rafienajwan <rafie.anjasmara@gmail.com>
2025-03-23 22:14:21 +0700

    fix(migrations): buat email dan username unique dan buat default untuk status

--- database/migrations/2025_03_22_135135_create_users_table.php
+++ database/migrations/2025_03_22_135135_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->char('password', 30);
-            $table->char('email', 30);
-            $table->char('username', 20);
+            $table->char('email', 30)->unique();
+            $table->char('username', 20)->unique();
             $table->char('nama', 30);
-            $table->enum('status', ['available', 'notavailable']);
+            $table->enum('status', ['available', 'notavailable'])->default('available');
             $table->string('desc', 500)->nullable();
             $table->date('birthdate');
             $table->char('fotoProfil', 100)->nullable();
